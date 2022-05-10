2f3d1ce701e2af82f027cfb54b7a77450c301033
BenjiChejtman <46895375@est.ort.edu.ar>
2022-05-10 12:09:10 -0300

    Final commit

---
+++ Assets/DataInputManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DataInputManager : MonoBehaviour
+{
+    public Text txtUserInput;
+
+    public void ShowGreetings()
+    {
+        Debug.Log(int.Parse(txtUserInput.text) * 2); //convertimos a int y multiplicamos x 2, ya que txtUserInput(.text) vuelve como tipo string.
+    }
+}
--- Assets/Instantiator.cs
+++ Assets/Instantiator.cs
@@ -1,31 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Instantiator : MonoBehaviour
 {
     public GameObject objectToClone;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-        
-    }
+    public int cloneAmount;
+    public Text txtCloneAmount;
+    GameObject clon;
 
-    // Update is called once per frame
-    void Update()
+    public void MultiInstantiate() //funcion de tipo void
     {
-        
-    }
+        cloneAmount = int.Parse(txtCloneAmount.text); //cloneamount = limite de counter y depsues lo convertimos a int
+        //int counter = 0;
 
-    public void TripleInstantiate()
-    {
-        int counter = 0;
-        while (counter < 3)
+        for (int counter = 0; counter < cloneAmount; counter++) //counter es lo mismo que i, osea podemos reemplazarlo por i.
         {
-            Instantiate(objectToClone);
-            counter++;
+            clon = Instantiate(objectToClone); //instantiate es el empty. Le dice que se clone. //instantia es una funcion de tipo gameobject, creo.
+            Destroy(clon, 1); //destruir clon despues de 1 segundo.
         }
     }
 }
